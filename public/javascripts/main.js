2a33b0f43bcec226c7457437ba7c8f4fbed6769b
Young Kim <ky200223@gmail.com>
2015-09-10 22:41:09 +0900

    [Develop] mobile news page, campus page

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -4,7 +4,7 @@
 
 $(document).ready(function () {
     $('#fullpage').fullpage({
-        anchors: ['introduction', 'news', 'campus', 'instruction', 'cooperation'],
+        anchors: ['introduction', 'news', 'instruction', 'campus', 'cooperation'],
         menu: '#menu',
         scrollingSpeed: 1000
     });
